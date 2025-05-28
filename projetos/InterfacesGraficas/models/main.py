e826314aad3d5a6e8d99b52859b143998f090bdb
Gustavo-Morchel <gustavomorchel@gmail.com>
2025-05-27 21:39:53 -0300

    Added more Python interface examples (definitely not for me)

--- projetos/InterfacesGraficas/models/main.py
+++ projetos/InterfacesGraficas/models/main.py
@@ -1,20 +1,33 @@
 import tkinter as tk
 
 
-def clique():
-    if rotulo.cget("text") == "Olá, Mundo!":
-        rotulo.config(text="Você clicou no botão!")
-    else:
-        rotulo.config(text="Olá, Mundo!")
+
+def mostrar_opcao(botao):
+    texto = f"Escolha {opcao1.get()}"
+    texto += f" {opcao2.get()}"
+    texto += f" {opcao3.get()}"
+    rotulo.config(text=texto)
+
+    if opcao1.get() and opcao2.get() and opcao3.get():
+        if botao == "Saude":
+            opcao1.set(False)
+        if botao == "Tempo":
+            opcao3.set(False)
+        if botao == "Dinheiro":
+            opcao2.set(False)    
 
 janela = tk.Tk()
-janela.title("Minha Janela")
+janela.title("Exemplo de Interface Gráfica")
+janela.geometry("300x200")
+opcao1 = tk.BooleanVar()
+opcao2 = tk.BooleanVar()
+opcao3 = tk.BooleanVar()
 
-rotulo = tk.Label(janela, text="Olá, Mundo!", font=("Arial", 16))
-rotulo.pack(pady=20)
+tk.Radiobutton(janela, text="Dinheiro", variable=opcao1, value=True, command=lambda : mostrar_opcao("Dinheiro")).pack()
+tk.Radiobutton(janela, text="Tempo", variable=opcao2, value=True, command=lambda : mostrar_opcao("Tempo")).pack()
+tk.Radiobutton(janela, text="Saude", variable=opcao3, value=True, command=lambda : mostrar_opcao("Saude")).pack()
 
-botao = tk.Button(janela, text="Clique aqui", command=clique)
-botao.pack(pady=10)
+rotulo = tk.Label(janela, text="Escolheu")
+rotulo.pack(pady=10)
 
-janela.geometry("400x300")
 janela.mainloop()
